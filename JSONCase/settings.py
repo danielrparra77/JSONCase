e87dc46c488df49b20fd99d8aff6a09e73ebf9d1
User <User@DANIELRPARRA77>
2015-12-16 18:54:40 -0500

    Comienzo de creacion de modelado de la base de datos desde la aplicacion en DJANGO de Devson

--- JSONCase/settings.py
+++ JSONCase/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'devson.apps.DevsonConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -70,14 +71,25 @@ TEMPLATES = [
 WSGI_APPLICATION = 'JSONCase.wsgi.application'
 
 
-# Database
+# Databases
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
 DATABASES = {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #},
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'devson',
+        'USER': 'postgres',
+        'PASSWORD': 'dans123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
+    
 }
 
 
--- devson/models.py
+++ devson/models.py
@@ -2,4 +2,27 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Proyecto(models.Model):
+    N_NombreProyecto = models.CharField(max_length=200)
+    V_FechaCreacion = models.DateTimeField('date published')
+
+class TipoObjeto(models.Model):
+    N_TipoObjeto = models.CharField(max_length=200)
+
+class Objeto(models.Model):
+    V_CoordenadaX = models.IntegerField()
+    V_CoordenadaY = models.IntegerField()
+    N_ClaseObjeto = models.CharField(max_length=4)
+    N_SiRaiz = models.BooleanField()
+    K_Proyecto = models.ForeignKey(Proyecto)
+    K_TipoObjeto = models.ForeignKey(TipoObjeto)
+    K_HijoDe = models.ForeignKey("self")
+
+class ValorObjeto(models.Model):
+    K_ValorObjeto = models.CharField(max_length=1000)
+    K_Objeto = models.ForeignKey(Objeto)
+
+class EstiloObjeto(models.Model):
+    V_TipoLetra = models.CharField(max_length=100)
+    V_Color = models.IntegerField()
+    K_Objeto = models.ForeignKey(Objeto)
