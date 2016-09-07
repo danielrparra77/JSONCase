e9d5349189fe245320d242445339cfe3ec6eccc2
EDUARDO VILLA <EDUARDO VILLA@DESKTOP-VJUOQQM>
2016-09-07 17:50:01 -0500

    ajuste area de trabajo

--- JSONCase/settings.py
+++ JSONCase/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'devson',
         'USER': 'postgres',
-        'PASSWORD': 'dans123',
+        'PASSWORD': 'fenix940326',
         'HOST': 'localhost',
         'PORT': '5432',
     }
